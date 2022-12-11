e2908f1b50e89ef36e746dbf854652592c21ba0e
stempnakowski <120311871+stempnakowski@users.noreply.github.com>
2022-12-11 13:03:46 +0100

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,20 @@
+def myfunc():
+		# global x
+		x = "awesome!"
+		print("Python is " + x)
+	
+x = "a pain in the ass."
+
+myfunc()
+print(" X is", x)
+
+y = 2.8
+print("y as float", y)
+print("y as int", int(y))
+
+a = "Hello, World!"
+print(a[1])
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
